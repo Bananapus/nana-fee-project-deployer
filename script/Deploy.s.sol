2b67eb4e732815edf6159192eb78987bc2e8efb4
mejango <mejango@protonmail.com>
2025-02-12 14:39:24 -0300

    more issuance

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -158,7 +158,7 @@ contract DeployScript is Script, Sphinx {
             startsAtOrAfter: uint40(block.timestamp + TIME_UNTIL_START),
             splitPercent: 6200, // 62%
             splits: splits,
-            initialIssuance: uint112(1000 * DECIMAL_MULTIPLIER),
+            initialIssuance: uint112(10_000 * DECIMAL_MULTIPLIER),
             issuanceCutFrequency: 360 days,
             issuanceCutPercent: 380_000_000, // 38%
             cashOutTaxRate: 1000, // 0.1
